d2b806d5d47683e4abb6536669afd59987433ded
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-02-18 13:02:49 +0800

    Tool

---
+++ Tool/Z.Tool.Icon.IconList/Value.cs
@@ -0,0 +1,10 @@
+namespace Z.Tool.Icon.IconList;
+
+class Value : Any
+{
+    public virtual bool Has016 { get; set; }
+    public virtual bool Has032 { get; set; }
+    public virtual bool Has064 { get; set; }
+    public virtual bool Has128 { get; set; }
+    public virtual bool Has256 { get; set; }
+}
