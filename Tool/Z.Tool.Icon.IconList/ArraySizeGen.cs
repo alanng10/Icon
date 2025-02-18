fee9a5e157e9b4a5a7cd8a1b08ef339b50053e83
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-02-18 12:13:49 +0800

    Tool

--- Tool/Z.Tool.Icon.IconList/ArraySizeGen.cs
+++ Tool/Z.Tool.Icon.IconList/ArraySizeGen.cs
@@ -1,9 +1,32 @@
 namespace Z.Tool.Icon.IconList;
 
-class ArraySizeGen : Any
+class ArraySizeGen : ToolBase
 {
-    public override bool Init()
+    public virtual Table IconTable { get; set; }
+
+    public virtual bool Execute()
     {
+        this.AddClear();
+
+        Iter iter;
+        iter = this.IconTable.IterCreate();
+
+        this.IconTable.IterSet(iter);
+
+        while (iter.Next())
+        {
+            String name;
+            name = iter.Index as String;
+
+            this.AddIndent(2).AddS("this.AddItem(this.IconList.").Add(name).AddS(");").AddLine();
+        }
+
+        String kk;
+        kk = this.AddResult();
+
+        String ka;
+        ka = this.StringInt(this.IconTable.Count);
+
         
 
         return true;
