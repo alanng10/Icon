b5b06721060aea6bb0799eff60c4d30dd5cd57eb
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-02-18 11:19:27 +0800

    Tool

--- Tool/Z.Tool.Icon.IconList/-.cs
+++ Tool/Z.Tool.Icon.IconList/-.cs
@@ -1,3 +1,6 @@
+global using String = Avalon.Infra.String;
+global using Array = Avalon.List.Array;
+global using StorageComp = Avalon.Storage.Comp;
 global using EntryEntry = Avalon.Entry.Entry;
 global using SourceGen = Z.Infra.ListSourceGen.Gen;
 global using STAThreadAttribute = System.STAThreadAttribute;
--- Tool/Z.Tool.Icon.IconList/Gen.cs
+++ Tool/Z.Tool.Icon.IconList/Gen.cs
@@ -5,6 +5,8 @@ public class Gen : SourceGen
     public override bool Init()
     {
         base.Init();
+        this.StorageComp = StorageComp.This;
+
         this.Module = this.S("Icon.Icon");
         this.ClassName = this.S("IconList");
         this.BaseClassName = this.S("Any");
@@ -15,6 +17,32 @@ public class Gen : SourceGen
         this.ItemListFileName = null;
         this.AddMethodFileName = this.S("ToolData/Icon/AddMaideIcon.txt");
         this.OutputFilePath = this.S("../../Module/Icon.Icon/IconList.cl");
+        return true;
+    }
+
+    protected virtual StorageComp StorageComp { get; set; }
+
+    protected override bool ExecuteItemList()
+    {
+        this.ItemTable = this.ToolInfra.TableCreateStringLess();
+
+        Array array032;
+        array032 = this.StorageComp.EntryList(this.S("../../../Crystal/032"), false);
+
+        long count;
+        count = array032.Count;
+
+        long i;
+        i = 0;
+        while (i < count)
+        {
+            String fileName;
+            fileName = array032.GetAt(i) as String;
+
+            i = i + 1;
+        }
+
+
         return true;
     }
 }
